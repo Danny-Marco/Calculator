39bdf111035f716d391f327f8864503707f263ec
Danny Marco <marco.denny7@gmail.com>
2021-01-19 01:42:09 +0600

    Реализовал получение и проверку математической операции от пользователя. Создал тестовый проект. Написал тесты на проверку мат. операции

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Calculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var oper = GetOperation();
+            Console.WriteLine(oper);
+        }
+
+        private static string GetOperation()
+        {
+            while (true)
+            {
+                Console.Write("Ввудите операцию: ");
+                var userInput = Console.ReadLine();
+                if (!CheckOperation(userInput))
+                {
+                    Console.WriteLine("Операция не валидна!\n");
+                }
+                else
+                {
+                    return userInput;
+                }
+            }
+        }
+
+        public static bool CheckOperation(string userInput)
+        {
+            userInput = userInput.Replace(" ", "");
+            var rgx = new Regex(@"^[0-9+\-*%\/\(\)]*$");
+            return rgx.IsMatch(userInput) && userInput.Length > 0 && !StartsWithMathSymbol(userInput);
+        }
+
+        private static bool StartsWithMathSymbol(string userInput)
+        {
+            var firstElement = userInput.First();
+            var lastElement = userInput.Last();
+            var firstElementIsSymbol =
+                firstElement == '+'
+                || firstElement == '/'
+                || firstElement == '%'
+                || firstElement == '*';
+            var lastElementIsSymbol =
+                lastElement == '+'
+                || lastElement == '-'
+                || lastElement == '/'
+                || lastElement == '%'
+                || lastElement == '*';
+            
+            return firstElementIsSymbol || lastElementIsSymbol;
         }
     }
 }
